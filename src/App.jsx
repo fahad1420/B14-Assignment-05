9cc56b850b98f5de08e022de75fabd1270f294a4
fahad1420 <rafahadrafahad01@gmail.com>
2026-09-13 01:14:34 +0300

    Set up React project foundation

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Dev Stack Builder</h1>
+      <p>Build your ideal development stack.</p>
+    </div>
+  );
+}
+
+export default App;
