998efe0f8e8d32c168e4364681a4105db813a011
fahad1420 <rafahadrafahad01@gmail.com>
2026-09-14 03:26:33 +0300

    Configure deployment paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/B14-Assignment-05/',
+  base: process.env.GITHUB_ACTIONS ? '/B14-Assignment-05/' : '/',
   plugins: [react()],
 })
